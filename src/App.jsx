a467805ea96f4519b76f02bdb9e4e31dbc4b877c
Mayank Pandey <immayank7781@gmail.com>
2024-12-01 23:38:15 +0530

    src

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <div className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
